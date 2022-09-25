85d7f985f200c3759b0af393ce2aea20cf3dd87a
Diratix <62076034+Diratix@users.noreply.github.com>
2022-09-25 19:16:57 +0200

    Created Whitelist
    Whitelist Issue

---
+++ src/main/java/org/mcadminToolkit/whitelist/whitelist.java
@@ -0,0 +1,37 @@
+package org.mcadminToolkit.whitelist;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.Set;
+
+public class whitelist {
+    public static String[] getWhiteList(JavaPlugin plugin){
+        Server server = plugin.getServer();
+        Set<String> whitelistTMP = new HashSet<String>();
+
+        Set<OfflinePlayer> whitelistPlayers = server.getWhitelistedPlayers();
+
+        for (OfflinePlayer player: whitelistPlayers){
+            String playerName = player.getName();
+            whitelistTMP.add(playerName);
+        }
+        return  whitelistTMP.toArray(new String[0]);
+    }
+
+    public static String addWhitelistPlayer(JavaPlugin plugin, String userName){
+        Server server = plugin.getServer();
+
+        try{
+            server.dispatchCommand(server.getConsoleSender(), "whitelist add " + userName);
+            server.reloadWhitelist();
+        }catch (Exception err){
+            return err.toString();
+        }
+
+        return "Success";
+    }
+}
