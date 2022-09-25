c390857575e5d1967ee3670baba90985d172d381
Diratix <62076034+Diratix@users.noreply.github.com>
2022-09-25 22:48:21 +0200

    Create serverStats.java
    Getting Server Stats Issue but for now without disk usage

---
+++ src/main/java/org/mcadminToolkit/serverStats/serverStats.java
@@ -0,0 +1,49 @@
+package org.mcadminToolkit.serverStats;
+
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
+public class serverStats {
+    private static String convert(long bytes) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absB < 1024) {
+            return bytes + " B";
+        }
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
+            ci.next();
+        }
+        value *= Long.signum(bytes);
+        return String.format("%.1f %ciB", value / 1024.0, ci.current());
+    }
+
+    public static String cpuUsage (JavaPlugin plugin){
+        Server server = plugin.getServer();
+        OperatingSystemMXBean system = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+        double cpuUsageDouble = system.getSystemCpuLoad();
+        String cpuUsage = Double.toString(cpuUsageDouble) + "%";
+
+        return cpuUsage;
+    }
+
+    public static String playersOnline (JavaPlugin plugin){
+        Server server = plugin.getServer();
+
+        Player[] onlinePlayers = server.getOnlinePlayers().toArray(new Player[0]);
+        int onlinePlayersCount = onlinePlayers.length;
+
+        int offlinePlayers = server.getMaxPlayers();
+
+        return Integer.toString(onlinePlayersCount) + "/" + Integer.toString(offlinePlayers);
+    }
+}
